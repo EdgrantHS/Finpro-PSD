d1ffbf312e65a17e6ce6c6448e2756b893191e8a
EdgrantHS <edgrant.h@gmail.com>
2023-12-13 20:40:19 +0700

    adder and int mul

---
+++ src/AdderFloat.vhd
@@ -0,0 +1,26 @@
+-- Ini adalah adder floating point dengan spesifikasi double dengan IEE 754
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+
+entity AdderFloat is
+    Port (
+        InAddA : in STD_LOGIC_VECTOR(31 downto 0);
+        InAddB : in STD_LOGIC_VECTOR(31 downto 0);
+        OutSum : out STD_LOGIC_VECTOR(31 downto 0);
+        OutOverflow : out STD_LOGIC;
+        clk, rst : in STD_LOGIC
+    );
+end AdderFloat;
+
+architecture Behavioral of AdderFloat is
+    signal Sum : unsigned(32 downto 0); -- Intermediate signal for sum, 33 bits to capture overflow
+begin
+    -- Dataflow style implementation
+    -- The output is directly defined as a function of the inputs
+    Sum <= ('0' & unsigned(InAddA)) + ('0' & unsigned(InAddB)); -- Extend inputs to 33 bits for overflow detection
+    OutSum <= std_logic_vector(Sum(31 downto 0)); -- Assign the lower 32 bits to OutSum
+    OutOverflow <= Sum(32); -- The 33rd bit indicates overflow
+
+end Behavioral;
---
+++ src/MultiplierInteger.vhd
@@ -0,0 +1,58 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity MultiplierInteger is
+    Port ( 
+        InMul1 : in STD_LOGIC_VECTOR(31 downto 0);
+        InMul2 : in STD_LOGIC_VECTOR(31 downto 0);
+        OutMul : out STD_LOGIC_VECTOR(31 downto 0);
+        MulOverflowFlag : out STD_LOGIC;
+        clk, rst : in STD_LOGIC
+    );
+end MultiplierInteger;
+
+architecture Behavioral of MultiplierInteger is
+    signal Result : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+    signal InAddA : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+    signal InAddB : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+    signal OutSum : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+    signal OutOverflow : STD_LOGIC := '0';
+    signal resetAdder : STD_LOGIC := '0';
+    signal doneMul : STD_LOGIC := '0';
+    signal addRepitition : integer := 0;
+    signal OutMultiplier : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+    signal multiplicant : integer := 0;
+    
+begin
+    multiplicant <= to_integer(unsigned(InMul2));
+
+    decoderEnt : entity work.AdderFloat 
+        port map (
+            InAddA => InAddA,
+            InAddB => InAddB,
+            OutSum => OutSum,
+            OutOverflow => OutOverflow,
+            clk => clk,
+            rst => resetAdder
+        );
+
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            OutMul <= (others => '0');
+            MulOverflowFlag <= '0';
+        elsif rising_edge(clk) then
+            if addRepitition = multiplicant then
+                doneMul <= '1';
+            else
+                doneMul <= '0';
+                InAddA <= InMul1;
+                InAddB <= OutMultiplier;
+                OutSum <= OutMultiplier;
+                addRepitition <= addRepitition + 1;
+            end if;
+        end if;
+    end process;
+      
+end Behavioral;
