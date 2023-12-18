5e67f25d79ede7981b7bc7beffb91eab526d5c96
EdgrantHS <edgrant.h@gmail.com>
2023-12-18 21:50:17 +0700

    tambahin direksi

--- src/FloatAdder.vhd
+++ src/FloatAdder.vhd
@@ -20,6 +20,63 @@ architecture rtl of FloatAdder is
     
 begin
     
-    
+-- // Helper Functions:
+-- Function getFraction(float32 number) -> (SLV(sign + mantissa)):
+--     // Extract sign and mantissa from the 32-bit floating-point number
+
+
+-- Function getExponent(float32 number) -> (SLV exponent):
+--     // Extract exponent from the 32-bit floating-point number
+
+-- Function ShiftRight(SLV mantissa, int shift):
+--     // Right shift/left the mantissa by 'shift' places, handling overflow/underflow
+--     ...
+
+-- Function Assemble(mantissa, exponent) -> float32:
+--     // Assemble the 32-bit floating-point number from sign, exponent, and mantissa
+
+
+
+-- main: process (clk, rst) 
+--     // Step 1: Decompose each number into sign, exponent, and mantissa
+--     signA, exponentA, mantissaA = Decompose(a)
+--     signB, exponentB, mantissaB = Decompose(b)
+
+--     // handle zeros
+--     if a and b are zeros:
+--         return zero
+
+--     // Step 3: Align the mantissas
+--     // This may involve shifting the mantissa of the number with the smaller exponent
+--     if exponentA > exponentB:
+--         ShiftRight(mantissaB, exponentA - exponentB)
+--         exponent = exponentA
+--     else:
+--         ShiftRight(mantissaA, exponentB - exponentA)
+--         exponent = exponentB
+
+--     // Step 4: Add or subtract the mantissas based on the signs
+--     if signA == signB:
+--         mantissa = mantissaA + mantissaB
+--         sign = signA
+--     else:
+--         if mantissaA > mantissaB:
+--             mantissa = mantissaA - mantissaB
+--             sign = signA
+--         else:
+--             mantissa = mantissaB - mantissaA
+--             sign = signB
+
+--     // Step 5: Normalize the result
+--     // Shift the mantissa and adjust the exponent until the mantissa is normalized
+--     while mantissa not in normalized range:
+--         if mantissa too large:
+--             ShiftRight(mantissa)
+--             exponent += 1
+--         else if mantissa too small:
+--             ShiftLeft(mantissa)
+--             exponent -= 1
+
+--     return result    
     
 end architecture rtl;
--- src/TopLevel.vhd
+++ src/TopLevel.vhd
@@ -4,9 +4,7 @@ use IEEE.numeric_std.all;
 
 entity TopLevel is
     port (
-        ControlWord : in std_logic_vector (64 downto 0);
-        FloatSum : out std_logic_vector (31 downto 0);
-        CarryFlag : out std_logic;    
+        
     );
 end entity TopLevel;
 
