d6bb910d758a83dfc8af69c266596ad14aefac14
NadhiefIlmi <fasichulilmi2003@gmail.com>
2023-12-23 20:37:46 +0700

    Nadhif

--- src/Adder.vhd
+++ src/Adder.vhd
@@ -4,6 +4,46 @@ use IEEE.numeric_std.all;
 
 entity Adder is
     port (
+        AddNumA : in std_logic_vector(31 downto 0);
+        AddNumB : in std_logic_vector(31 downto 0);
+        Sum : out std_logic_vector(31 downto 0);
+        CarryFlag : out std_logic
+    );
+end entity Adder;
+
+architecture rtl of Adder is
+    function FullAdderBit(A, B, Cin : std_logic) return std_logic_vector is
+        variable SumBit : std_logic_vector(0 to 1);
+    begin
+        SumBit(0) := A xor B xor Cin;
+        SumBit(1) := (A and B) or (B and Cin) or (A and Cin);
+        return SumBit;
+    end function FullAdderBit;
+
+    signal Carry : std_logic := '0';
+
+begin
+    process (AddNumA, AddNumB)
+        variable TempSum : std_logic_vector(31 downto 0) := (others => '0');
+        variable TempCarry : std_logic := '0';
+    begin
+        for i in 0 to 31 loop
+            TempSum(i) := FullAdderBit(AddNumA(i), AddNumB(i), TempCarry)(0);
+            TempCarry := FullAdderBit(AddNumA(i), AddNumB(i), TempCarry)(1);
+        end loop;
+
+        Sum <= TempSum;
+        CarryFlag <= TempCarry;
+    end process;
+end architecture rtl;
+
+
+/*library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity Adder is
+    port (
         AddNumA : in std_logic_vector (31 downto 0);
         AddNumB : in std_logic_vector (31 downto 0);
         Sum : out std_logic_vector (31 downto 0);
--- src/FloatAdder.vhd
+++ src/FloatAdder.vhd
@@ -4,6 +4,116 @@ use IEEE.numeric_std.all;
 
 entity FloatAdder is
     port (
+        FloatNumA   : in std_logic_vector (31 downto 0);
+        FloatNumB   : in std_logic_vector (31 downto 0);
+        clk         : in std_logic;
+        rst         : in std_logic;
+        SignSelect  : in std_logic;
+        FloatOut    : out std_logic_vector (31 downto 0);
+        CarryFlag   : out std_logic;
+        Done        : out std_logic
+    );
+end entity FloatAdder;
+
+architecture rtl of FloatAdder is
+    function getFraction(number : std_logic_vector) return std_logic_vector is
+        variable result : std_logic_vector(31 downto 0);
+    begin
+      
+        return result;
+    end function;
+
+    function getExponent(number : std_logic_vector) return std_logic_vector is
+        variable result : std_logic_vector(7 downto 0);
+    begin
+       
+        return result;
+    end function;
+
+    function ShiftRight(mantissa : std_logic_vector; shift : integer) return std_logic_vector is
+        variable result : std_logic_vector(mantissa'length downto 0);
+    begin
+      
+        return result;
+    end function;
+
+    function Assemble(sign : std_logic; exponent : std_logic_vector; mantissa : std_logic_vector) return std_logic_vector is
+        variable result : std_logic_vector(31 downto 0);
+    begin
+     
+        return result;
+    end function;
+
+    
+
+    signal signA, signB : std_logic;
+    signal exponentA, exponentB : std_logic_vector(7 downto 0);
+    signal mantissaA, mantissaB : std_logic_vector(23 downto 0);
+    signal exponent, mantissa : std_logic_vector(7 downto 0);
+
+begin
+    main: process (clk, rst)
+    begin
+        if rst = '1' then
+            
+        elsif rising_edge(clk) then
+            -- Step 1: Decompose each number into sign, exponent, and mantissa
+            signA     <= getFraction(FloatNumA)(31);
+            exponentA <= getExponent(FloatNumA);
+            mantissaA <= getFraction(FloatNumA)(22 downto 0);
+
+            signB     <= getFraction(FloatNumB)(31);
+            exponentB <= getExponent(FloatNumB);
+            mantissaB <= getFraction(FloatNumB)(22 downto 0);
+
+            -- handle zeros
+            if (FloatNumA = "00000000000000000000000000000000") and (FloatNumB = "00000000000000000000000000000000") then
+                FloatOut <= (others => '0');
+                CarryFlag <= '0';
+                Done <= '1';
+            else
+                -- Step 3: Align the mantissas
+                if exponentA > exponentB then
+                    mantissaB <= ShiftRight(mantissaB, to_integer(unsigned(exponentA) - unsigned(exponentB)));
+                    exponent  <= exponentA;
+                else
+                    mantissaA <= ShiftRight(mantissaA, to_integer(unsigned(exponentB) - unsigned(exponentA)));
+                    exponent  <= exponentB;
+                end if;
+
+                -- Step 4: Add or subtract the mantissas based on the signs
+                if signA = signB then
+                    mantissa <= std_logic_vector(unsigned(mantissaA) + unsigned(mantissaB));
+                    sign <= signA;
+                else
+                    if mantissaA > mantissaB then
+                        mantissa <= std_logic_vector(unsigned(mantissaA) - unsigned(mantissaB));
+                        sign <= signA;
+                    else
+                        mantissa <= std_logic_vector(unsigned(mantissaB) - unsigned(mantissaA));
+                        sign <= signB;
+                    end if;
+                end if;
+
+                -- Step 5: Normalize the result
+                
+                FloatOut <= Assemble(sign, exponent, mantissa);
+                CarryFlag <= '0';
+                Done <= '1';     
+            end if;
+        end if;
+    end process main;
+
+end architecture rtl;
+
+
+
+/*library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity FloatAdder is
+    port (
         FloatNumA : in std_logic_vector (31 downto 0);
         FloatNumB : in std_logic_vector (31 downto 0);
         clk : in std_logic;
